1ca8b26d4ae608931eada8def03639c7cea5fcff
lucas-souza-visio <lucas.souza@visio.com.br>
2022-11-26 17:49:09 -0300

    Corrigindo port

--- config/server.js
+++ config/server.js
@@ -12,7 +12,7 @@ app.use(serverSession({
     saveUninitialized: true
 }));
 
-const port = process.ENV.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Servidor rodando na porta: ', port);
